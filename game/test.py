f21d574b0f8fb17f01360707116e5be2f266c35f
Chris Palmer <capalmer1013@gmail.com>
2015-04-18 22:23:08 -0400

    Flying Dorito
    !!!!!!!

--- game/test.py
+++ game/test.py
@@ -12,10 +12,6 @@ def rot_center(image, angle):
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
 
-def point_pos(x0, y0, d, theta):
-    theta_rad = pi/2 - radians(theta)
-    return x0 + d*cos(theta_rad), y0 + d*sin(theta_rad)
-
 
 def point_position(x0, y0, dist, theta):
     return dist*sin(theta), dist*cos(theta)
@@ -23,59 +19,83 @@ def point_position(x0, y0, dist, theta):
 pygame.init()
 
 window_size = width, height = 640, 480
-black = 0, 0, 0
+black = 255, 255, 255
 
 speed = [0, 0]
-yChange = 50
-xChange = 50
+playerMaxSpeed = 50
+playerSpeed = playerMaxSpeed
+distance = 20
+playerY = 50
+playerX = 50
 screen = pygame.display.set_mode(window_size)
 
+move = False
 rotate_left = False
 rotate_right = False
 rotate_counter_start = 20
 rotate_counter = rotate_counter_start
-player = pygame.image.load("models/Player-test-model.png")
+player = pygame.image.load("models/Player-Triangle-00.png")
 
-player.set_alpha(0)
+angleChange = 0
+
+player.set_alpha(255)
 
 playerRect = player.get_rect()
 rot_tuple = (player, playerRect)
 
 while True:
-    # bullshit starts here
     playerRect = playerRect.move(speed)
     if playerRect.left < 0 or playerRect.right > width:
         speed[0] = -speed[0]
+
     if playerRect.top < 0 or playerRect.bottom > height:
         speed[1] = -speed[1]
+
     if rotate_right:
         rotate_counter -= 1
         if rotate_counter == 0:
-            player = rot_center(player, -10)
+            angleChange -= 10
             rotate_counter = rotate_counter_start
+
     if rotate_left:
         rotate_counter -= 1
         if rotate_counter == 0:
-            player = rot_center(player, 10)
+            angleChange += 10
             rotate_counter = rotate_counter_start
+    if move:
+        xyTuple = point_position(0, 0, distance, radians(angleChange))
+        playerSpeed -= 1
+        if playerSpeed == 0:
+            playerSpeed = playerMaxSpeed
+            playerX -= xyTuple[0]
+            playerY -= xyTuple[1]
+
 
     # bullshit ends here
     for event in pygame.event.get():
         if event.type == KEYDOWN and event.key == K_LEFT:
             rotate_left = True
+
         if event.type == KEYUP and event.key == K_LEFT:
             rotate_left = False
+
         if event.type == KEYUP and event.key == K_RIGHT:
             rotate_right = False
 
         if event.type == KEYDOWN and event.key == K_RIGHT:
             rotate_right = True
+
         if event.type == KEYDOWN and event.key == K_UP:
-            yChange -= 10
+            move = True
+
+        if event.type == KEYUP and event.key == K_UP:
+            move = False
 
         if event.type == QUIT:
             exit()
+    angleChange = angleChange % 360
 
+    tempPlayer = rot_center(player, angleChange)
     screen.fill(black)
-    screen.blit(player, (xChange, yChange))
+    screen.blit(tempPlayer, (playerX, playerY))
     pygame.display.flip()
